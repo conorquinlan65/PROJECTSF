ae39bcb1edd391eda45a84c8267887d923866e59
Conor Quinlan <Conor.Quinlan@du.edu>
2024-04-01 23:58:06 -0500

    More Changes

--- web-dev-portfolio/tailwind.config.js
+++ web-dev-portfolio/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,11 +12,6 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      ...colors,
-      primary: colors.purple,
-      secondary: colors.pink,
     },
   },
   plugins: [],
